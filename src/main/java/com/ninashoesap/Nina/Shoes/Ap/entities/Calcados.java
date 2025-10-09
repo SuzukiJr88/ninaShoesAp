d49c46c752136f4cccd1928cf989a14f0752c1e4
SuzukiJr <36607603+SuzukiJr88@users.noreply.github.com>
2025-10-09 17:00:44 -0300

    Criando Entities

---
+++ src/main/java/com/ninashoesap/Nina/Shoes/Ap/entities/Calcados.java
@@ -0,0 +1,133 @@
+package com.ninashoesap.Nina.Shoes.Ap.entities;
+
+import java.util.Objects;
+
+import com.ninashoesap.Nina.Shoes.Ap.entities.enums.TipoCalcado;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_calcados")
+public class Calcados {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nomeSapato;
+	private TipoCalcado tipoCalcado;
+	private Integer qtd33;
+	private Integer qtd34;
+	private Integer qtd35;
+	private Integer qtd36;
+	private Integer qtd37;
+	private Integer qtd38;
+	private Integer qtd39;
+	private Integer qtd40;
+
+	//Getter and Setters
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getNomeSapato() {
+		return nomeSapato;
+	}
+	public void setNomeSapato(String nomeSapato) {
+		this.nomeSapato = nomeSapato;
+	}
+	public TipoCalcado getTipoCalcado() {
+		return tipoCalcado;
+	}
+	public void setTipoCalcado(TipoCalcado tipoCalcado) {
+		this.tipoCalcado = tipoCalcado;
+	}
+	public Integer getQtd33() {
+		return qtd33;
+	}
+	public void setQtd33(Integer qtd33) {
+		this.qtd33 = qtd33;
+	}
+	public Integer getQtd34() {
+		return qtd34;
+	}
+	public void setQtd34(Integer qtd34) {
+		this.qtd34 = qtd34;
+	}
+	public Integer getQtd35() {
+		return qtd35;
+	}
+	public void setQtd35(Integer qtd35) {
+		this.qtd35 = qtd35;
+	}
+	public Integer getQtd36() {
+		return qtd36;
+	}
+	public void setQtd36(Integer qtd36) {
+		this.qtd36 = qtd36;
+	}
+	public Integer getQtd37() {
+		return qtd37;
+	}
+	public void setQtd37(Integer qtd37) {
+		this.qtd37 = qtd37;
+	}
+	public Integer getQtd38() {
+		return qtd38;
+	}
+	public void setQtd38(Integer qtd38) {
+		this.qtd38 = qtd38;
+	}
+	public Integer getQtd39() {
+		return qtd39;
+	}
+	public void setQtd39(Integer qtd39) {
+		this.qtd39 = qtd39;
+	}
+	public Integer getQtd40() {
+		return qtd40;
+	}
+	public void setQtd40(Integer qtd40) {
+		this.qtd40 = qtd40;
+	}
+
+	//Constructor
+	
+	public Calcados(String nomeSapato, TipoCalcado tipoCalcado, Integer qtd33, Integer qtd34, Integer qtd35,
+			Integer qtd36, Integer qtd37, Integer qtd38, Integer qtd39, Integer qtd40) {
+		this.nomeSapato = nomeSapato;
+		this.tipoCalcado = tipoCalcado;
+		this.qtd33 = qtd33;
+		this.qtd34 = qtd34;
+		this.qtd35 = qtd35;
+		this.qtd36 = qtd36;
+		this.qtd37 = qtd37;
+		this.qtd38 = qtd38;
+		this.qtd39 = qtd39;
+		this.qtd40 = qtd40;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Calcados other = (Calcados) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
---
+++ src/main/java/com/ninashoesap/Nina/Shoes/Ap/entities/enums/TipoCalcado.java
@@ -0,0 +1,13 @@
+package com.ninashoesap.Nina.Shoes.Ap.entities.enums;
+
+public enum TipoCalcado {
+
+	BOTA,
+	TENIS,
+	SAPATO,
+	RASTEIRINHA,
+	SALTO,
+	SAPATILHA,
+	OUTROS;
+	
+}
