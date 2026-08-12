97a5dda86189328a8075441199adb9a93ecb5c03
SuzukiJr <36607603+SuzukiJr88@users.noreply.github.com>
2026-08-12 19:24:56 -0300

    H2 Database, test profile, JPA

--- src/main/java/com/ninashoesap/Nina/Shoes/Ap/entities/Calcados.java
+++ src/main/java/com/ninashoesap/Nina/Shoes/Ap/entities/Calcados.java
@@ -15,6 +15,11 @@ import jakarta.persistence.Table;
 @Table(name = "tb_calcados")
 public class Calcados implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
